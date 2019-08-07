687ef4d2f50e61cf7a3998e4e9b63ca85ffb21fe
tb812657_2010 <isongliang@qq.com>
2019-08-07 22:12:03 +0800

    123

--- src/main/java/com/joy/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/joy/apigateway/ApiGatewayApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-
-
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
